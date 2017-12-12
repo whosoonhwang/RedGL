12b9f5ff0f1bb0a83e3127a40bff3030bf4ce24d
백선기 <webseon@gmail.com>
2017-12-12 12:59:46 +0900

    query가 빌드에 포함안되어있어서 포함

---
+++ beta/rnd_shader_maker/Structure_Texture.js
@@ -0,0 +1,107 @@
+
+'use strict';
+var Structure_Texture;
+// 텍스쳐 기본정보
+(function () {
+    var W;
+    var targetContainer;
+    var startX, startY
+    W = 150
+    Structure_Texture = function (tInfo) {
+        var rootBox;
+        var inputBox;
+        var outputBox;
+        var info;
+        ////////////////////////////////////////////////////////
+        info = tInfo
+        
+        ////////////////////////////////////////////////////////
+        rootBox = Recard.Dom('div').S(
+            'position', 'absolute',
+            'left', Recard.WIN.w / 2 - W / 2,
+            'top', Recard.WIN.h / 2,
+            'width', 150,
+            'min-height', 100,
+            'background', '#444',
+            'box-shadow', '0px 0px 10px 10px rgba(0,0,0,0.1)',
+            '>', Recard.Dom('div').S(
+                'background', '#666',
+                'height', 30,
+                'line-height', 30,
+                'padding-left', 6,
+                'html', info['title'] ? info['title'] : '정의되지않았음',
+                'cursor', 'pointer',
+                'on', ['down', function (e) {
+                    targetContainer = rootBox
+                    startX = e.nativeEvent.offsetX
+                    startY = e.nativeEvent.offsetY
+                }]
+            ),
+            '>', inputBox = Recard.Dom('div').S(
+                'float', 'left',
+                'display', 'inline-block',
+                'width', '50%',
+            ),
+            '>', outputBox = Recard.Dom('div').S(
+                'float', 'right',
+                'display', 'inline-block',
+                'width', '50%'
+            )
+        )
+        for (var k in info['structure']['input']) {
+            Recard.Dom('div').S(
+                '@className', 'inputItem',
+                '@key', k,
+                'html', k,
+                '>', Recard.Dom('div').S(
+                    'position', 'absolute',
+                    'top', '50%',
+                    'left', 0,
+                    'width', 10,
+                    'height', 10,
+                    'transform', 'translate(-50%,-50%)',
+                    'background', '#fff',
+                    'border-radius', '50%'
+                ),
+                '<', inputBox
+            )
+        }
+        for (var k in info['structure']['output']) {
+            Recard.Dom('div').S(
+                '@className', 'outputItem',
+                '@key', k,
+                'html', k,
+                '>', Recard.Dom('div').S(
+                    'position', 'absolute',
+                    'top', '50%',
+                    'right', 0,
+                    'width', 10,
+                    'height', 10,
+                    'transform', 'translate(50%,-50%)',
+                    'background', '#fff',
+                    'border-radius', '50%'
+                ),
+                '<', outputBox
+            )
+        }
+        ////////////////////////////////////////////////////////
+        rootBox['info'] = info
+        rootBox['next'] = null
+        rootBox['prev'] = null
+        ////////////////////////////////////////////////////////
+
+        return rootBox
+    }
+    Object.freeze(Structure_Texture)
+    Recard.EVENT_EMITTER.on(window, 'mousemove', function (e) {
+        if (targetContainer) {
+            targetContainer.S(
+                'top', Recard.WIN.mouseY - startY,
+                'left', Recard.WIN.mouseX - startX
+            )
+        }
+    })
+    Recard.EVENT_EMITTER.on(window, 'mouseup', function (e) {
+        targetContainer = null
+    })
+})();
--- beta/rnd_shader_maker/index.js
+++ beta/rnd_shader_maker/index.js
@@ -1,2 +1,68 @@
 'use strict';
 
+Recard.static('INDEX', (function () {
+    var result;
+    result = {
+        init: function () {
+            Recard.Css('body').S(
+                'background', '#222',
+                'font-size', 12,
+                'color', '#fff',
+                'user-select', 'none'
+            )
+            Recard.Css('.inputItem').S(
+                'position', 'relative',
+                'text-align', 'left',
+                'padding-left', 10,
+                'height', 20,
+                'font-size', 11,
+                'line-height', 20
+            )
+            Recard.Css('.outputItem').S(
+                'position', 'relative',
+                'text-align', 'right',
+                'padding-right', 10,
+                'height', 20,
+                'font-size', 11,
+                'line-height', 20
+            )
+            /////////////////////////////////////
+            var info;
+            info = {}
+            info['structure'] = {
+                title : null,
+                output: {
+                    TEXTURE: null,
+                    R: null,
+                    G: null,
+                    B: null,
+                    A: null
+                },
+                input: {
+                    UV: null
+                }
+            }
+            info.src = ''
+            /////////////////////////////////////
+            var diffuse, normal;
+            diffuse = new Structure_Texture(info)
+            normal = new Structure_Texture(info)
+            console.log(diffuse)
+
+            diffuse['next'] = [normal,'UV']
+            normal['prev'] = [diffuse,'TEXTURE']
+
+
+            Recard.LOOPER.add('test',function(){
+                // console.log(diffuse['next'],normal['prev'])
+                diffuse['next'].query('[key="UV"]')
+            })
+
+            Recard.Dom('body').S(
+                '>', diffuse,
+                '>', normal
+            )
+        }
+    }
+    return result
+})())
