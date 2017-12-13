f664cd90acd87b30b5700db321d4cc1800312cb9
백선기 <webseon@gmail.com>
2017-12-13 09:38:23 +0900

    타입체크를 어떻게 할지 고민해야하는군...

--- beta/rnd_shader_maker/index.js
+++ beta/rnd_shader_maker/index.js
@@ -4,7 +4,7 @@ Recard.static('INDEX', (function () {
     var result;
     var svgList = []
     result = {
-        getLineList : function(){
+        getLineList: function () {
             return svgList
         },
         init: function () {
@@ -35,53 +35,133 @@ Recard.static('INDEX', (function () {
             /////////////////////////////////////
             var info;
             info = {}
+            info['type'] = 'Texture'
             info['structure'] = {
                 title: null,
                 output: {
-                    TEXTURE: null,
-                    R: null,
-                    G: null,
-                    B: null,
-                    A: null
+                    TEXTURE: 'vec4',
+                    R: 'float',
+                    G: 'float',
+                    B: 'float',
+                    A: 'float'
                 },
                 input: {
-                    UV: null,
+                    UV: 'vec2',
                     TEST: null
                 }
             }
             info.src = ''
             /////////////////////////////////////
             var diffuse, normal;
-            diffuse = new Structure_Texture(info)
-            normal = new Structure_Texture(info)
+            diffuse = new Structure_Node(info)
+            normal = new Structure_Node(info)
             console.log(diffuse)
 
             diffuse.S('left', 100)
             normal.S('left', 400)
-            
-            Recard.UPDATER.init()
-            
 
-            Recard.Dom('button').S(
-                'position','absolute',
-                'bottom',10,
-                'left',10,
-                'z-index',1,
-                'padding',10,
-                'background','#5b52aa',
-                'color','#fff',
-                'outline','none',
-                'border',0,
-                'cursor','pointer',
-                'html','텍스쳐추가',
+            Recard.UPDATER.init()
+
+            Recard.Dom('div').S(
+                'position', 'absolute',
+                'bottom', 10,
+                'left', 10,
+                'right', 10,
                 '<','body',
-                'on',['down',function(){
-                    console.log('test'),
-                    (new Structure_Texture(info)).S(
-                        '<','body'
-                    )
-                }]
+                '>', Recard.Dom('button').S(
+                    'margin-right',1,
+                    'padding', 10,
+                    'background', '#5b52aa',
+                    'color', '#fff',
+                    'outline', 'none',
+                    'border', 0,
+                    'cursor', 'pointer',
+                    'html', '텍스쳐추가',
+                    'on', ['down', function () {
+                        var info;
+                        info = {}
+                        info['type'] = 'Texture'
+                        info['structure'] = {
+                            title: null,
+                            output: {
+                                TEXTURE: 'vec4',
+                                R: 'float',
+                                G: 'float',
+                                B: 'float',
+                                A: 'float'
+                            },
+                            input: {
+                                UV: 'vec2',
+                                TEST: null
+                            }
+                        }
+                        info.src = '';
+                        (new Structure_Node(info)).S(
+                            '<', 'body'
+                        )
+                    }]
+                ),
+                '>', Recard.Dom('button').S(
+                    'margin-right',1,
+                    'padding', 10,
+                    'background', '#5b52aa',
+                    'color', '#fff',
+                    'outline', 'none',
+                    'border', 0,
+                    'cursor', 'pointer',
+                    'html', 'Add',
+
+                    'on', ['down', function () {
+                        var info;
+                        info = {}
+                        info['type'] = 'Add'
+                        info['structure'] = {
+                            title: null,
+                            output: {
+                                OUTPUT: null
+                            },
+                            input: {
+                                INPUT1: null,
+                                INPUT2: null
+                            }
+                        }
+                        info.src = '';
+                        (new Structure_Node(info)).S(
+                            '<', 'body'
+                        )
+                    }]
+                ),
+                '>', Recard.Dom('button').S(
+                    'margin-right',1,
+                    'padding', 10,
+                    'background', '#5b52aa',
+                    'color', '#fff',
+                    'outline', 'none',
+                    'border', 0,
+                    'cursor', 'pointer',
+                    'html', 'Mul',
+                    'on', ['down', function () {
+                        var info;
+                        info = {}
+                        info['type'] = 'Mul'
+                        info['structure'] = {
+                            title: null,
+                            output: {
+                                OUTPUT: null
+                            },
+                            input: {
+                                INPUT1: null,
+                                INPUT2: null
+                            }
+                        }
+                        info.src = '';
+                        (new Structure_Node(info)).S(
+                            '<', 'body'
+                        )
+                    }]
+                )
             )
+
 
             Recard.Dom('body').S(
                 '>', diffuse,
