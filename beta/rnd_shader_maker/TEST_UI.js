1dc1d11b4056b3376614ec2558957606ee503dec
백선기 <webseon@gmail.com>
2017-12-14 12:17:07 +0900

    맵별로 어떻게 심을지 논리만 나오면 1차 연구는 끝내고.. 실사용버전으로 재작성함..

--- beta/rnd_shader_maker/TEST_UI.js
+++ beta/rnd_shader_maker/TEST_UI.js
@@ -2,6 +2,7 @@
 Recard.static('TEST_UI', (function () {
     var result;
     var setResult
+    var nodeBox;
     setResult = function () {
         var info;
         info = {}
@@ -20,7 +21,8 @@ Recard.static('TEST_UI', (function () {
         var result;
         result = new Structure_Node(info)
         result.S(
-            'left', 800,
+            'top',10000,
+            'left', 10000,
             '<', 'body'
         )
     }
@@ -98,25 +100,52 @@ Recard.static('TEST_UI', (function () {
                         mergeCompileSource['define']['vars'] = mergeCompileSource['define']['vars'].concat(
                             item['rootBox']['compileSourceInfo']['define']['vars']
                         )
-                        
-                      
                     })
                 }
                 console.log(mergeCompileSource)
-                root.makeCode(mergeCompileSource)
+                var ttt = root.makeCode(mergeCompileSource)
+                Recard.PREVIEW.setTest(null,ttt,mergeCompileSource['define']['uniforms'])
 
 
             }
         })(),
         init: function () {
+            (function(){
+                var startX,startY
+                nodeBox= Recard.Dom('div').S(
+                    '@id','nodeBox',
+                    'position','relative',
+                    'width',20000,
+                    'height',20000,
+                    '<','body',
+                    'on',['down',function(){
+                        startX = Recard.WIN.scroll('x')
+                        startY = Recard.WIN.scroll('y')
+                        this.S(
+                            'on',['move',function(e){
+                                console.log('오니2',e)
+                                startX-=e.nativeEvent.movementX
+                                startY-=e.nativeEvent.movementY
+                                Recard.WIN.scroll(startX,startY)
+                            }]
+                        )
+                    }],
+                    'on',['up',function(){
+                        this.S(
+                            'off','move'
+                        )
+                    }]
+                )
+            })();
+            Recard.WIN.scroll(nodeBox.__dom__.clientWidth/2-Recard.WIN.w/2,nodeBox.__dom__.clientHeight/2-Recard.WIN.h/2)
             setResult()
             /////////////////////////////////////
             Recard.Dom('div').S(
-                'position', 'absolute',
+                'position', 'fixed',
                 'bottom', 10,
                 'left', 10,
                 'right', 10,
-                'z-index', 10000000,
+                'z-index', 20000000,
                 '<', 'body',
                 '>', Recard.Dom('button').S(
                     'margin-right', 1,
@@ -147,6 +176,8 @@ Recard.static('TEST_UI', (function () {
                         }
                         info.src = '';
                         (new Structure_Node(info)).S(
+                            'top',Recard.WIN.scroll('y')+Recard.WIN.h/2,
+                            'left',Recard.WIN.scroll('x')+Recard.WIN.w/2,
                             '<', 'body'
                         )
                     }]
