e8fbd1ccc3b84c5579e99d7bf9110f0e3c780db7
백선기 <webseon@gmail.com>
2017-12-27 10:08:36 +0900

    1212

--- beta2/glsl/skyBoxFS.glsl
+++
@@ -1,6 +0,0 @@
-precision lowp float;
-varying vec3 vCubeCoord;
-uniform samplerCube uSkybox;
-void main() {
-    gl_FragColor = textureCube(uSkybox, vCubeCoord);
-}
--- beta2/rnd_shader_maker/trash/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-shaderMixer(
-    [
-        VS_BITMAP_BASE_SET,
-        FS_BITMAP_BASE_SET
-    ]
-)
